2fc974696f50306bf38c9c4d3ad492b84c4afb9c
Jake <Jake>
2020-05-10 13:57:22 +0100

    Main now calls the controller.Run with a pre-configured queue to watch configmap resources

--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 
 	"github.com/aclevername/config-map-controller/log"
 
+	"github.com/aclevername/config-map-controller/processor"
+
+	"github.com/aclevername/config-map-controller/controller"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	log.SetLevel(0)
+	annotation := "x-k8s.io/curl-me-that"
 
 	kubeconfig := flag.String("kubeconfig", "", "path to kubeconfig")
 	flag.Parse()
@@ -27,9 +36,29 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = kubernetes.NewForConfig(config)
+	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Error("failed to build kube client from: %s", *kubeconfig)
 		os.Exit(1)
 	}
+
+	configMapListWatcher := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "configmaps", v1.NamespaceAll, fields.Everything())
+
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	_, informer := cache.NewIndexerInformer(configMapListWatcher, &v1.ConfigMap{}, 0, cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			queue.Add(obj)
+		},
+		UpdateFunc: func(old interface{}, new interface{}) {
+			queue.Add(new)
+		}}, cache.Indexers{})
+
+	p := processor.New(clientset, annotation)
+	configMapController := controller.NewConfigMapController(queue, informer, &p)
+
+	stopCh := make(chan struct{})
+
+	log.Debug("starting controller to watch for %s annotation", annotation)
+	configMapController.Run(stopCh)
 }
